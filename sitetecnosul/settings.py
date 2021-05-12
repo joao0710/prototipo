e760301a056210980b7b88fb9a856572d0d73cbf
joao_victor <joao0710@hotmail.com>
2021-05-12 15:07:30 -0300

    Instalação do Python-decouple

--- sitetecnosul/settings.py
+++ sitetecnosul/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#v_$4!*rr15^4ui7+@prj44z1r(552xl^mue4sq+a-gm%gb^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
