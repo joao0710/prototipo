200d2dae9b5d06cf52adf0a5905e65277d11cb07
joao_victor <joao0710@hotmail.com>
2021-05-31 18:03:48 -0300

    Encapsulamento da url

--- sitetecnosul/base/tests/test_home.py
+++ sitetecnosul/base/tests/test_home.py
@@ -5,7 +5,7 @@ from sitetecnosul.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    resp = client.get(reverse('home'))
+    resp = client.get(reverse('base:home'))
     return resp
 
 def test_status_code(resp):
@@ -15,4 +15,4 @@ def test_title(resp):
     assert_contains(resp, '<title>Tecnosul</title>')
 
 def test_home_link(resp):
-    assert_contains(resp, f'href="{reverse("home")}">Home</a>')
+    assert_contains(resp, f'href="{reverse("base:home")}">Tecnosul</a>')
